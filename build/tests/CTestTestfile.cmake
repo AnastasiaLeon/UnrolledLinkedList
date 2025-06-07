ffeff631cbcc5efcf458e32912f28b043850251c
Anastasiia Leonova <137497155+AnastasiaLeon@users.noreply.github.com>
2025-06-08 01:10:44 +0300

    Delete build/tests directory

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/anastasialeonova/Desktop/github projects/UnrolledLinkedList/tests
-# Build directory: /Users/anastasialeonova/Desktop/github projects/UnrolledLinkedList/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/anastasialeonova/Desktop/github projects/UnrolledLinkedList/build/tests/unrolled-list-lib-tests[1]_include.cmake")
-subdirs("../_deps/googletest-build")
